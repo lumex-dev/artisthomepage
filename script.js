a3b3f19adb5397b2a377c96d8a97e5c02fa4edc0
Moritz <72605546+baymoritz@users.noreply.github.com>
2026-01-15 12:53:17 +0100

    added texts and showcases

--- script.js
+++ script.js
@@ -8,33 +8,36 @@ burger.addEventListener("click", () => {
 
 // ====== 1) Daten: nur Datum + Ort/Titel + optional SoundCloud href ======
 const gigs = [
+    { date: "2025-10-01", title: "Time", href: "https://soundcloud.com/moritzminoa/moritz-minoa-time?in=moritzminoa/sets/own-tracks&si=519a63ec6b3d4b4eb72771e735dd0461&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
+    { date: "2025-10-01", title: "Awaken", href: "https://soundcloud.com/moritzminoa/moritz-minoa-awaken?in=moritzminoa/sets/own-tracks&si=ec07e267c6904db490d7d607da5d774a&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
+    { date: "2025-05-01", title: "Simultaneous Vibe", href: "https://soundcloud.com/moritzminoa/simultaneous-vibe-1?in=moritzminoa/sets/own-tracks&si=f8092f51eea341e882c894a0b071a9a2&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" },
     { date: "2026-09-26", title: "Private Hochzeit" },
     { date: "2026-06-13", title: "Private Hochzeit" },
-    { date: "2026-02-07", title: "Palais München" },
-    { date: "2026-01-24", title: "Korajo Winter Afterall, München" },
-    { date: "2026-01-23", title: "Zur Gruam, München" },
+    { date: "2026-02-07", title: "Palais, Munich" },
+    { date: "2026-01-24", title: "Korajo Winter Afterall, Munich" },
+    { date: "2026-01-23", title: "Zur Gruam, Munich" },
 
-    { date: "2025-10-03", title: "Bahnwärther Thiel" },
+    { date: "2025-10-03", title: "Bahnwärther Thiel, Munich" },
 
     {
         date: "2025-09-05",
-        title: "Festival am Waldrand",
+        title: "Festival am Waldrand, Pforzheim",
         href: "https://soundcloud.com/moritzminoa/moritz-minoa-kuckucksnest-festival-am-waldrand-2025-09-119-133-bpm",
     },
     {
         date: "2025-08-05",
-        title: "Bucht der Träumer* Festival",
+        title: "Bucht der Träumer* Festival, Helenesee",
         href: "https://soundcloud.com/moritzminoa/oniva-b2b-moritz-minoa-bucht-der-traumer-2025",
     },
 
-    { date: "2025-08-09", title: "Süss. War Gestern" },
-    { date: "2025-07-05", title: "Legal Club" },
-    { date: "2025-07-04", title: "Palais Club" },
-    { date: "2025-06-21", title: "Zur Gruam" },
-    { date: "2025-05-30", title: "Zur Gruam" },
-    { date: "2025-04-29", title: "Süss. War Gestern" },
-    { date: "2025-03-22", title: "Forsthaus Starnberg", href: "https://soundcloud.com/moritzminoa/2025-03-22-moritz-minoa-forsthaus" },
-    { date: "2025-02-07", title: "Lieberscholli Club", href: "https://soundcloud.com/moritzminoa/2025-03-07-luis-m" },
+    { date: "2025-08-09", title: "Süss. War Gestern, Berlin" },
+    { date: "2025-07-05", title: "Legal, Munich" },
+    { date: "2025-07-04", title: "Palais Club. Munich" },
+    { date: "2025-06-21", title: "Zur Grua, Munich" },
+    { date: "2025-05-30", title: "Zur Gruam, Munich" },
+    { date: "2025-04-29", title: "Süss. War Gestern, Berlin" },
+    { date: "2025-03-22", title: "Forsthaus, Starnberg", href: "https://soundcloud.com/moritzminoa/2025-03-22-moritz-minoa-forsthaus" },
+    { date: "2025-02-07", title: "Lieberscholli, Munich", href: "https://soundcloud.com/moritzminoa/2025-03-07-luis-m" },
 ];
 
 // ====== 2) Helpers ======
@@ -70,7 +73,7 @@ function formatLabel(dateStr, isFutureOrNext) {
 
 function soundcloudIconSvg() {
     return `
-    <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+    <svg class="icon soundcloudIcon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
       <path d="M11.56,8.87V17H20.32V17C22.17,16.87 23,15.73 23,14.33C23,12.85 21.88,11.66 20.38,11.66C20,11.66 19.68,11.74 19.35,11.88C19.11,9.54 17.12,7.71 14.67,7.71C13.5,7.71 12.39,8.15 11.56,8.87M10.68,9.89C10.38,9.71 10.06,9.57 9.71,9.5V17H11.1V9.34C10.95,9.5 10.81,9.7 10.68,9.89M8.33,9.35V17H9.25V9.38C9.06,9.35 8.87,9.34 8.67,9.34C8.55,9.34 8.44,9.34 8.33,9.35M6.5,10V17H7.41V9.54C7.08,9.65 6.77,9.81 6.5,10M4.83,12.5C4.77,12.5 4.71,12.44 4.64,12.41V17H5.56V10.86C5.19,11.34 4.94,11.91 4.83,12.5M2.79,12.22V16.91C3,16.97 3.24,17 3.5,17H3.72V12.14C3.64,12.13 3.56,12.12 3.5,12.12C3.24,12.12 3,12.16 2.79,12.22M1,14.56C1,15.31 1.34,15.97 1.87,16.42V12.71C1.34,13.15 1,13.82 1,14.56Z"/>
     </svg>
   `;
@@ -196,3 +199,4 @@ document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("timelineRoot");
     renderTimeline(gigs, root);
 });
+
